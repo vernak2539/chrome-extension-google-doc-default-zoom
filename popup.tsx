9023aa584bc640f34b8074b85038e654298bd809
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-01-25 11:43:05 +0000

    first pass at implementing zoom select

--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,35 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
+
+import { Storage } from "~node_modules/@plasmohq/storage"
+
+const storage = new Storage()
+
+// todo
+// - select selected things based on saved value
+
+const zoomValues = ["Fit", "50%", "75%", "125%", "150%", "200%"]
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [defaultZoom, setDefaultZoom] = useState("100%")
+
+  const onChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setDefaultZoom(event.target.value)
+  }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div>
+      <h1>Default Google Docs Zoom</h1>
+      <p>
+        Select the default zoom that you'd like Google Docs to use when loading.
+      </p>
+      {/*// how to load this dynamically*/}
+      <select onChange={onChange}>
+        {zoomValues.map((value) => {
+          const isSelected = value === defaultZoom
+
+          return <option selected={isSelected}>{value}</option>
+        })}
+      </select>
     </div>
   )
 }
