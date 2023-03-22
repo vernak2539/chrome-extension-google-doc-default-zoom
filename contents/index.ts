c964e1e87524d17d447f12e85112aabc3fd1ca05
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-03-22 15:28:19 +0000

    create strategies - prep for extended support

--- contents/index.ts
+++ contents/index.ts
@@ -2,18 +2,9 @@ import type { PlasmoCSConfig } from "plasmo"
 
 import { relay, sendViaRelay } from "@plasmohq/messaging"
 
-import {
-  CLICKABLE_ZOOM_OPTION_CLASS,
-  CLICKABLE_ZOOM_SELECT_ID,
-  DEFAULT_ZOOM,
-  OBSERVE_EXECUTION_LIMIT
-} from "~constants"
+import { OBSERVE_EXECUTION_LIMIT } from "~constants"
 import counterFactory from "~counter-factory"
-import {
-  getDOMElement,
-  getDOMElementCoordinates,
-  simulateClick
-} from "~ui-helpers"
+import DocsStrategy from "~src/strategies/docs"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://docs.google.com/*"]
@@ -24,7 +15,7 @@ relay({
   name: "get-zoom-value"
 })
 
-const getZoomValue = () => {
+const getZoomValue: () => Promise<string> = () => {
   return new Promise((resolve) => {
     sendViaRelay({ name: "get-zoom-value" }).then((response) => {
       resolve(response.zoomValue)
@@ -32,58 +23,11 @@ const getZoomValue = () => {
   })
 }
 
-const changeZoom = (zoomValue) => {
-  // don't do anything if zoom level is set to default value
-  if (zoomValue === DEFAULT_ZOOM) {
-    return
-  }
-
-  // get menu element responsible for changing zoom
-  const zoomInput = getDOMElement(CLICKABLE_ZOOM_SELECT_ID)
-  const { x: zoomInputX, y: zoomInputY } = getDOMElementCoordinates(zoomInput)
-  simulateClick(zoomInput, zoomInputX, zoomInputY)
-
-  // get zoom menu element dropdown
-  const zoomInputAriaOwns = zoomInput.attributes["aria-owns"].value // this is the link
-  const zoomInputSelect = getDOMElement(
-    `.goog-menu.goog-menu-vertical[aria-activedescendant="${zoomInputAriaOwns}"]`
-  )
-
-  // figure out zoom value to select
-  const zoomInputSelectOptions = zoomInputSelect.querySelectorAll(
-    CLICKABLE_ZOOM_OPTION_CLASS
-  )
-  let newZoomLevelElement = null
-  for (let i = 0; i < zoomInputSelectOptions.length; i++) {
-    if (zoomInputSelectOptions[i].firstChild.textContent === zoomValue) {
-      newZoomLevelElement = zoomInputSelectOptions[i].firstChild
-    }
-  }
-
-  // somehow we may not have matched the right element
-  if (!newZoomLevelElement) {
-    return
-  }
-
-  // select new zoom level
-  const { x: newZoomOptionX, y: newZoomOptionY } =
-    getDOMElementCoordinates(newZoomLevelElement)
-  simulateClick(newZoomLevelElement, newZoomOptionX, newZoomOptionY)
-
-  // close dropdown with blur event (may need to check again to see if it's closed)
-  setTimeout(() => {
-    simulateClick(getDOMElement("canvas"), 0, 0)
-  }, 500)
-}
-
-const getIsZoomSelectUIDisabled = () => {
-  const zoomSelect = getDOMElement(CLICKABLE_ZOOM_SELECT_ID)
-  return zoomSelect.classList.contains("goog-toolbar-combo-button-disabled")
-}
+const strategy = new DocsStrategy({ isViewOnly: false })
 
 const counter = counterFactory()
 const observer = new MutationObserver((_mutationList, observer) => {
-  const zoomIsDisabled = getIsZoomSelectUIDisabled()
+  const zoomIsDisabled = strategy.getIsZoomSelectorDisabled()
   const isExecutionCountOverLimit = counter.getCount() > OBSERVE_EXECUTION_LIMIT
 
   if (isExecutionCountOverLimit) {
@@ -93,7 +37,7 @@ const observer = new MutationObserver((_mutationList, observer) => {
 
   if (!zoomIsDisabled) {
     getZoomValue().then((zoomValue) => {
-      changeZoom(zoomValue)
+      strategy.execute(zoomValue)
     })
 
     observer.disconnect()
@@ -103,7 +47,7 @@ const observer = new MutationObserver((_mutationList, observer) => {
 })
 
 // initial kick-off
-const zoomIsDisabled = getIsZoomSelectUIDisabled()
+const zoomIsDisabled = strategy.getIsZoomSelectorDisabled()
 
 if (zoomIsDisabled) {
   observer.observe(document.getElementById("docs-toolbar"), {
@@ -112,6 +56,6 @@ if (zoomIsDisabled) {
   })
 } else {
   getZoomValue().then((zoomValue) => {
-    changeZoom(zoomValue)
+    strategy.execute(zoomValue)
   })
 }
---
+++ src/strategies/docs.ts
@@ -0,0 +1,78 @@
+import {
+  getDOMElement,
+  getDOMElementCoordinates,
+  simulateClick
+} from "~ui-helpers"
+
+interface BaseStrategy {
+  execute: (zoomValue: string) => void
+  getIsZoomSelectorDisabled: () => void
+}
+
+export const STORAGE_KEY = "zoomValue"
+export const ZOOM_VALUES = ["Fit", "50%", "75%", "100%", "125%", "150%", "200%"]
+export const DEFAULT_ZOOM = ZOOM_VALUES.at(3)
+export const OBSERVE_EXECUTION_LIMIT = 1000
+
+// UI Selectors
+export const CLICKABLE_ZOOM_SELECT_ID = "#zoomSelect"
+export const CLICKABLE_ZOOM_OPTION_CLASS = ".goog-menuitem"
+
+class DocsStrategy implements BaseStrategy {
+  private isViewOnly: boolean
+
+  constructor({ isViewOnly }) {
+    this.isViewOnly = isViewOnly
+  }
+
+  getIsZoomSelectorDisabled() {
+    const zoomSelect = getDOMElement(CLICKABLE_ZOOM_SELECT_ID)
+    return zoomSelect.classList.contains("goog-toolbar-combo-button-disabled")
+  }
+
+  execute(zoomValue: string) {
+    // don't do anything if zoom level is set to default value
+    if (zoomValue === DEFAULT_ZOOM) {
+      return
+    }
+
+    // get menu element responsible for changing zoom
+    const zoomInput = getDOMElement(CLICKABLE_ZOOM_SELECT_ID)
+    const { x: zoomInputX, y: zoomInputY } = getDOMElementCoordinates(zoomInput)
+    simulateClick(zoomInput, zoomInputX, zoomInputY)
+
+    // get zoom menu element dropdown
+    const zoomInputAriaOwns = zoomInput.attributes["aria-owns"].value // this is the link
+    const zoomInputSelect = getDOMElement(
+      `.goog-menu.goog-menu-vertical[aria-activedescendant="${zoomInputAriaOwns}"]`
+    )
+
+    // figure out zoom value to select
+    const zoomInputSelectOptions = zoomInputSelect.querySelectorAll(
+      CLICKABLE_ZOOM_OPTION_CLASS
+    )
+    let newZoomLevelElement = null
+    for (let i = 0; i < zoomInputSelectOptions.length; i++) {
+      if (zoomInputSelectOptions[i].firstChild.textContent === zoomValue) {
+        newZoomLevelElement = zoomInputSelectOptions[i].firstChild
+      }
+    }
+
+    // somehow we may not have matched the right element
+    if (!newZoomLevelElement) {
+      return
+    }
+
+    // select new zoom level
+    const { x: newZoomOptionX, y: newZoomOptionY } =
+      getDOMElementCoordinates(newZoomLevelElement)
+    simulateClick(newZoomLevelElement, newZoomOptionX, newZoomOptionY)
+
+    // close dropdown with blur event (may need to check again to see if it's closed)
+    setTimeout(() => {
+      simulateClick(getDOMElement("canvas"), 0, 0)
+    }, 500)
+  }
+}
+
+export default DocsStrategy
