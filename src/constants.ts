f4ea3fea8fa762884fec0cbd0587f7ac660962cd
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-03-22 15:48:19 +0000

    clean up constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,2 @@
 export const STORAGE_KEY = "zoomValue"
 export const OBSERVE_EXECUTION_LIMIT = 1000
-
-// UI Selectors
-export const CLICKABLE_ZOOM_SELECT_ID = "#zoomSelect"
-export const CLICKABLE_ZOOM_OPTION_CLASS = ".goog-menuitem"
