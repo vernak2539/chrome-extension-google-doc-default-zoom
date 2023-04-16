9238bfec2932405f047c764c84011bb1ccf57e91
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-04-16 14:06:52 +0100

    split things out

--- src/strategies/base.ts
+++ src/strategies/base.ts
@@ -1,4 +1,5 @@
 import type { UiStrategyConfig } from "../types"
+import getIsCustomZoom from "../utils/get-is-custom-zoom"
 import getZoomValueFromStorage from "../utils/get-zoom-value-from-storage"
 import {
   getDOMElement,
@@ -34,6 +35,16 @@ export abstract class AbstractBaseStrategy implements AbstractBaseStrategyImpl {
     if (!zoomValue || zoomValue === this.config.zoom.defaultZoom) {
       return
     }
+
+    if (getIsCustomZoom(zoomValue)) {
+      this.uiExecuteDefinedZoomFlow(zoomValue)
+    } else {
+      this.uiExecuteCustomZoomFlow(zoomValue)
+    }
+  }
+
+  private uiExecuteDefinedZoomFlow(zoomValue: string) {
+    // will need to use the help menu most likely... trusted keyboard events cannot be done
 
     // get menu element responsible for changing zoom
     const zoomInput = getDOMElement(
@@ -73,6 +84,103 @@ export abstract class AbstractBaseStrategy implements AbstractBaseStrategyImpl {
     setTimeout(() => {
       simulateClick(getDOMElement("canvas"), 0, 0)
     }, 500)
+  }
+
+  private uiExecuteCustomZoomFlow(zoomValue: string) {
+    // debugger
+    //
+    // zoomInput.addEventListener("keydown", (e) => {
+    //   console.log(e)
+    // })
+    //
+    // const inputBox = zoomInput.querySelector("input")
+    // inputBox.addEventListener("keydown", (e) => {
+    //   console.log(e)
+    // })
+    //
+    // inputBox.value = "130"
+    // inputBox.select()
+    // inputBox.dispatchEvent(new Event("input"))
+    // inputBox.dispatchEvent(new Event("input"))
+
+    // const zoomInput = document.querySelector('.docs-menu-input');
+    // zoomInput.focus();
+    // zoomInput.value = '150';
+    // zoomInput.dispatchEvent(new Event('input'));
+    // const keydownEvent = new KeyboardEvent("keydown", {
+    //   key: "Enter",
+    //   bubbles: true,
+    //   cancelable: true
+    // })
+    // inputBox.dispatchEvent(keydownEvent)
+    //
+    // // Create and dispatch an input event
+    // const inputEvent = new Event("input", {
+    //   bubbles: true,
+    //   cancelable: true
+    // })
+    // inputBox.dispatchEvent(inputEvent)
+    //
+    // // Create and dispatch a keyup event for the Enter key
+    // const keyupEvent = new KeyboardEvent("keyup", {
+    //   key: "Enter",
+    //   bubbles: true,
+    //   cancelable: true
+    // })
+    // inputBox.dispatchEvent(keyupEvent)
+
+    // const ev = new KeyboardEvent("keydown", {
+    //   view: window,
+    //   bubbles: true,
+    //   cancelable: true,
+    //   code: "Enter",
+    //   key: "Enter",
+    //   keyCode: 13,
+    //   which: 13
+    //   // clientX: 1,
+    //   // clientY: 1
+    // })
+    // Object.defineProperty(ev, "isTrusted", { value: true })
+    //
+    // console.log(ev)
+    // inputBox.dispatchEvent(ev)
+    // inputBox.dispatchEvent(
+    //   new KeyboardEvent("keyup", {
+    //     view: window,
+    //     bubbles: true,
+    //     cancelable: true,
+    //     code: "Enter",
+    //     key: "Enter",
+    //     keyCode: 13,
+    //     which: 13
+    //     // clientX: 1,
+    //     // clientY: 1
+    //   })
+    // )
+    // inputBox.dispatchEvent(
+    //   new KeyboardEvent("keypress", {
+    //     view: window,
+    //     bubbles: true,
+    //     cancelable: true,
+    //     key: "Enter"
+    //     // clientX: 1,
+    //     // clientY: 1
+    //   })
+    // )
+    // inputBox.dispatchEvent(
+    //   new KeyboardEvent("keyup", {
+    //     view: window,
+    //     bubbles: true,
+    //     cancelable: true,
+    //     key: "Enter"
+    //
+    //     // clientX: 1,
+    //     // clientY: 1
+    //   })
+    // )
+
+    // stop for now when the thing is open
+    return
   }
 
   public getIsZoomSelectorDisabled() {
