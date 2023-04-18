eb34e70e130f6e150400b9caf5e8fb1b8690468c
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-04-18 12:07:29 +0100

    move ui execution back to point in time where original input is used

--- src/strategies/base.ts
+++ src/strategies/base.ts
@@ -37,28 +37,31 @@ export abstract class AbstractBaseStrategy implements AbstractBaseStrategyImpl {
       return
     }
 
+    // get menu element responsible for changing zoom
+    const zoomInputContainer = getDOMElement(
+      this.config.uiElements.clickableZoomSelectId
+    )
+    const { x: zoomInputContainerX, y: zoomInputContainerY } =
+      getDOMElementCoordinates(zoomInputContainer)
+    simulateClick(zoomInputContainer, zoomInputContainerX, zoomInputContainerY)
+
     // add check for if strategy is supported or not for app
     if (getIsCustomZoom(zoomValue)) {
-      this.uiExecuteCustomZoomFlow(zoomValue)
+      this.uiExecuteCustomZoomFlow(zoomInputContainer, zoomValue)
     } else {
-      this.uiExecuteDefinedZoomFlow(zoomValue)
+      this.uiExecuteDefinedZoomFlow(zoomInputContainer, zoomValue)
     }
   }
 
-  private uiExecuteDefinedZoomFlow(zoomValue: string) {
-    // will need to use the help menu most likely... trusted keyboard events cannot be done
-
-    // get menu element responsible for changing zoom
-    const zoomInput = getDOMElement(
-      this.config.uiElements.clickableZoomSelectId
-    )
-    const { x: zoomInputX, y: zoomInputY } = getDOMElementCoordinates(zoomInput)
-    simulateClick(zoomInput, zoomInputX, zoomInputY)
-
+  private uiExecuteDefinedZoomFlow(
+    zoomInputContainer: Element,
+    zoomValue: string
+  ) {
     // get zoom menu element dropdown
-    const zoomInputAriaOwns = zoomInput.attributes["aria-owns"].value // this is the link
+    const zoomInputContainerAriaOwns =
+      zoomInputContainer.attributes["aria-owns"].value // this is the link
     const zoomInputSelect = getDOMElement(
-      `.goog-menu.goog-menu-vertical[aria-activedescendant="${zoomInputAriaOwns}"]`
+      `.goog-menu.goog-menu-vertical[aria-activedescendant="${zoomInputContainerAriaOwns}"]`
     )
 
     // figure out zoom value to select
@@ -88,47 +91,14 @@ export abstract class AbstractBaseStrategy implements AbstractBaseStrategyImpl {
     }, 500)
   }
 
-  private uiExecuteCustomZoomFlow(zoomValue: string) {
-    // THIS IS IMPOSSIBLE AT THE TIME DUE TO NOT BEING ABLE TO TRIGGER EVENTS BASED ON INPUTS CHANGING
-    // WILL BE LEAVING THIS HERE FOR NOW...
-
-    // open help menu
-    const helpMenu = getDOMElement(this.config.uiElements.toolbarHelpMenuId)
-    const { x: zoomInputX, y: zoomInputY } = getDOMElementCoordinates(helpMenu)
-    simulateClick(helpMenu, zoomInputX, zoomInputY)
-
-    // - find the dropdown due to client height - document.querySelectorAll(".docs-omnibox-input.jfk-textinput.label-input-label")
-
-    // find the dropdown that is visible after clicking "help"
-    const allDropdownMenus = document.querySelectorAll(
-      ".goog-menu.goog-menu-vertical.docs-material.ia-menu.ia-primary-menu"
-    )
-    console.log(allDropdownMenus)
-    const helpDropdownMenuIndex = Array.from(allDropdownMenus).findIndex(
-      (menu) => Boolean(menu.clientHeight)
-    )
-    const helpDropdownMenu = allDropdownMenus[helpDropdownMenuIndex]
-
-    console.log(helpDropdownMenu)
-    console.log(helpDropdownMenu.querySelectorAll("input"))
-
-    const helpDropdownInput = helpDropdownMenu.querySelectorAll("input")[0]
-
-    // type in "zoom 113"
-    setTimeout(() => {
-      helpDropdownInput.value = `zoom ${getNumericZoom(zoomValue)}`
-      // helpDropdownInput.select()
-      helpDropdownInput.focus()
-
-      helpDropdownInput.dispatchEvent(new InputEvent("input"))
-      helpDropdownInput.dispatchEvent(
-        new KeyboardEvent("keyup", { key: "Enter" })
-      )
-    }, 1000)
-
-    // set small timeout for dropdown / observer
-
-    // click first thing in dropdown
+  private uiExecuteCustomZoomFlow(
+    zoomInputContainer: Element,
+    zoomValue: string
+  ) {
+    // remove percentage value, convert back to string
+    const zoomInput = zoomInputContainer.querySelector("input")
+    zoomInput.value = getNumericZoom(zoomValue).toString()
+  }
 
     // thing should close
   }
