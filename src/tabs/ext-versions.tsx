b2bd77738c95d25df4a75272bce094a722989df6
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-05-24 15:56:13 +0100

    initial tab page detailing versions of the extension

---
+++ src/tabs/ext-versions.tsx
@@ -0,0 +1,104 @@
+import localize from "../utils/localize"
+
+type Data = {
+  supported: boolean
+  name: string
+}
+const CheckList = ({ title, data }) => {
+  return (
+    <>
+      <p>
+        <strong>{title}:</strong>
+      </p>
+      <ul style={{ listStyle: "none", margin: 0, padding: "0 0 0 15px" }}>
+        {data.map((d) => (
+          <li>
+            {d.supported ? "✅" : "❌"} {d.name}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+const RequiredPermissions = ({ permissions }) => {
+  return (
+    <>
+      <p>
+        <strong>Chrome Extension Permissions Required:</strong>
+      </p>
+      <p>{permissions.length > 0 ? permissions.join(", ") : "None"}</p>
+    </>
+  )
+}
+
+const supportedWorkspaceApps: Data[] = [
+  {
+    name: "Google Docs",
+    supported: true
+  },
+  {
+    name: "Google Sheets",
+    supported: true
+  }
+]
+
+const extDefaultFunctionality: Data[] = [
+  {
+    name: localize("ExtVersionsTabFunctionalityIncludedStandardZoom"),
+    supported: true
+  },
+  {
+    name: localize("ExtVersionsTabFunctionalityIncludedCustomZoom"),
+    supported: false
+  }
+]
+
+const extExtendedFunctionality: Data[] = [
+  {
+    name: localize("ExtVersionsTabFunctionalityIncludedStandardZoom"),
+    supported: true
+  },
+  {
+    name: localize("ExtVersionsTabFunctionalityIncludedCustomZoom"),
+    supported: true
+  }
+]
+
+const ExtensionVersionsPage = () => {
+  return (
+    <>
+      <h1>{localize("ExtVersionsTabHeader")}</h1>
+      <h2>{localize("extensionName")}</h2>
+      <CheckList
+        title={localize("ExtVersionsTabAppsSupportedHeader")}
+        data={supportedWorkspaceApps}
+      />
+      <CheckList
+        title={localize("ExtVersionsTabFunctionalityIncludedHeader")}
+        data={extDefaultFunctionality}
+      />
+      <RequiredPermissions permissions={[]} />
+      <br />
+      <h2>{localize("extensionNameExtended")}</h2>
+      <CheckList
+        title={localize("ExtVersionsTabAppsSupportedHeader")}
+        data={supportedWorkspaceApps}
+      />
+      <CheckList
+        title={localize("ExtVersionsTabFunctionalityIncludedHeader")}
+        data={extExtendedFunctionality}
+      />
+      <RequiredPermissions
+        permissions={["`debugger` (this is an elevated permission)"]}
+      />
+      <small>
+        <strong>
+          <em>{localize("ExtVersionsTabExtendedPermissionExplanation")}</em>
+        </strong>
+      </small>
+    </>
+  )
+}
+
+export default ExtensionVersionsPage
