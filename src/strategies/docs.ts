d1dfbedb769fbaf218476d836be8bf60968a5f26
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-07-24 13:43:27 +0100

    making progress

--- src/strategies/docs.ts
+++ src/strategies/docs.ts
@@ -1,6 +1,12 @@
+import { getClosestZoomValue } from "src/utils/get-closest-zoom-value"
 import type { ExecutionLocation, UiStrategyConfig } from "../types"
 import type { AbstractBaseStrategyImpl } from "./base"
 import { AbstractBaseStrategy } from "./base"
+import {
+  getDOMElement,
+  getDOMElementCoordinates,
+  simulateClick
+} from "src/utils/ui-helpers"
 
 class DocsStrategy
   extends AbstractBaseStrategy
@@ -30,9 +36,26 @@ class DocsStrategy
 
   private uiExecuteDocsViewOnlyFlow(zoomValue: string) {
     // view only Docs don't allow for custom zoom levels, but rather only allow the predefined ones
+    let finalZoom = zoomValue
+
     // 1. check if zoom value is included in predefined list
-    // 2. if not, find the closes zoom value (bias to towards higher)\
+    const isPredefinedList = this.config.zoom.zoomValues.includes(
+      zoomValue as never
+    )
+
+    // 2. if not, find the closest zoom value in the predefined list (bias to towards higher)
+    if (!isPredefinedList) {
+      finalZoom = getClosestZoomValue(this.config.zoom.zoomValues, zoomValue)
+    }
+
     // 3. set zoom value (new flow)
+    const viewMenuBarBtn = getDOMElement(
+      this.config.uiElements.menubarViewTabId
+    )
+    const { x: viewMenuBarBtnX, y: viewMenuBarBtnY } =
+      getDOMElementCoordinates(viewMenuBarBtn)
+
+    simulateClick(viewMenuBarBtn, viewMenuBarBtnX, viewMenuBarBtnY)
 
     console.log("VIEW ONLY")
   }
