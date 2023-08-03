856384d3f4e4f2e5693b449d802312ee48bc70d9
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-08-03 12:49:17 +0100

    translations for experimental features tab

--- src/tabs/experimental-features.tsx
+++ src/tabs/experimental-features.tsx
@@ -2,12 +2,16 @@ import localize from "../utils/localize"
 
 const ExperimentalVersionsPage = () => {
   return (
-    <>
+    <div style={{ maxWidth: "500px" }}>
       <h1>{localize("ExperimentalFeaturesTabHeader")}</h1>
       <section id="docs-view-only">
-        <h2>{localize("experimentalFeatureDocsViewOnlyHeader")}</h2>
+        <h2>{localize("ExperimentalFeaturesTabDocsViewOnlyHeader")}</h2>
+        <p>{localize("ExperimentalFeaturesTabDocsViewOnlyContent1")}</p>
+        <p>{localize("ExperimentalFeaturesTabDocsViewOnlyContent2")}</p>
+        <p>{localize("ExperimentalFeaturesTabDocsViewOnlyContent3")}</p>
+        <p>{localize("ExperimentalFeaturesTabDocsViewOnlyContent4")}</p>
       </section>
-    </>
+    </div>
   )
 }
 
