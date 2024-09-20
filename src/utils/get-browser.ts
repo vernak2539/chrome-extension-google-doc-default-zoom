a2ff5ede761afcba1fbe24d19d433806e549129d
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2024-09-20 10:12:47 +0100

    fix(edge): show extension version infomation in edge (and chrome)

--- src/utils/get-browser.ts
+++ src/utils/get-browser.ts
@@ -1,3 +1,11 @@
 export const isChrome = () => {
   return process.env.PLASMO_BROWSER === "chrome";
 };
+
+export const isFirefox = () => {
+  return process.env.PLASMO_BROWSER === "firefox";
+};
+
+export const isEdge = () => {
+  return process.env.PLASMO_BROWSER === "edge";
+};
