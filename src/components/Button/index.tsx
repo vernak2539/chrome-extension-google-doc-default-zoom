fee68d681acc39161b46d81d392710e99e4f89a9
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2024-10-01 14:38:28 +0100

    fix(button): add children to button and remove comments

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,21 +3,16 @@ import { Button as BaseButton, type ButtonProps } from "react-aria-components";
 import * as styles from "./styles.module.css";
 
 interface Props extends ButtonProps {
-  label: string;
+  label?: string;
   variant: "primary" | "danger";
 }
 
-const Button = ({ label, variant, ...props }: Props) => {
+const Button = ({ label, variant, children, ...props }: Props) => {
   const buttonStyles = classnames(styles.button, styles[variant]);
-
-  // {
-  //   [styles.primary]: variant === "primary",
-  //   [styles.danger]: variant === "danger"
-  // });
 
   return (
     <BaseButton {...props} className={buttonStyles}>
-      {label}
+      {children || label}
     </BaseButton>
   );
 };
