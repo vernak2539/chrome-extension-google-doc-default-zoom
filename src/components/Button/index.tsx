6a218253ba2e2f0ff279a58831a0664b5c6a48cc
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2024-10-02 12:38:53 +0100

    feat(button): use Button and update blues

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,6 +10,8 @@ interface Props extends ButtonProps {
 const Button = ({ label, variant, children, ...props }: Props) => {
   const buttonStyles = classnames(styles.button, styles[variant]);
 
+  console.log(BaseButton);
+
   return (
     <BaseButton {...props} className={buttonStyles}>
       {children || label}
