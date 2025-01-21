49ab84dc8665a2b6deb75346d538fbda73122045
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2025-01-21 14:28:11 +0000

    fix(logger): revert to regular console.log for logging

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -1,45 +1,30 @@
 import type { ExtensionFileSource } from "src/types";
-import { createLogger, format, transports, type Logger } from "winston";
-const { combine, timestamp, label } = format;
+
+const PREFIX = "Default Zoom GW - ";
 
 class BrowserLogger {
-  logger: Logger;
   contexts: [string, string][];
 
   constructor(source: ExtensionFileSource) {
     this.contexts = [["Source", source]];
-    this.logger = createLogger(this.getOptions());
-  }
-
-  private getOptions() {
-    return {
-      level: "info",
-      format: this.format(),
-      transports: [new transports.Console()]
-    };
-  }
-
-  private format() {
-    const labels = this.contexts.reduce((acc, [key, value]) => {
-      acc.push(label({ label: `${key}=${value}` }));
-      return acc;
-    }, []);
-
-    return combine(timestamp(), ...labels);
   }
 
   public addContext(key: string, value: string) {
     this.contexts.push([key, value]);
-    this.logger.configure(this.getOptions());
   }
 
-  public log(message: string) {
+  public info(message: string) {
     const context = this.contexts.reduce((acc, [key, value]) => {
       acc[key] = value;
       return acc;
     }, {});
 
-    this.logger.info(message, context);
+    console.log(
+      `%c${PREFIX}%c${message} - %c${JSON.stringify(context, null, 2)}`,
+      "color: blue;font-weight: bold;",
+      "color: green;",
+      "color: red;"
+    );
   }
 }
 
