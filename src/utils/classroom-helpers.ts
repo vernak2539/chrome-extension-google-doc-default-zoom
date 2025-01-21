f4116f3a57b4b3f2f1a48f1dd2d4ff670116e773
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2025-01-21 15:36:19 +0000

    fix(classroom): add function to determine if googleclassroom domain

--- src/utils/classroom-helpers.ts
+++ src/utils/classroom-helpers.ts
@@ -4,3 +4,20 @@ export const isGoogleClassroomDomain = () => {
 
   return isGoogleClassroomDomain;
 };
+
+export const isGoogleClassroomSubmittedAssignment = () => {
+  const url = window.location.href;
+  // const allowedApps = ["Docs", "Sheets"];
+  // const currentApp = getCurrentApp(url);
+
+  // if (!allowedApps.includes(currentApp)) {
+  //   return false;
+  // }
+
+  // This is a kind of URL loaded from a submitted assignment that is loaded in an iframe
+  // https://docs.google.com/document/d/1SdKXcvnUYO_SBhFl-zG1CJ01gFdO0KntJMy_rPghE3c/grading?allow_parent=https://classroom.google.com&enable_comments=true&tab=t.0
+  const { pathname } = new URL(url);
+  return pathname.endsWith("/grading");
+
+  return true;
+};
