7b634a74d69e5a9c47ada2b6f457595c312f011a
Alex Vernacchia <vernak2539@users.noreply.github.com>
2025-02-10 21:51:54 +0000

    feat(logger): use tslog

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -1,12 +1,23 @@
 import type { ExtensionFileSource } from "src/types";
+import type { ILogObj, Logger as LoggerType } from "tslog";
+import * as tslog from "tslog/dist/esm/index.js";
+
+const { Logger } = tslog;
 
 const PREFIX = "[Default Zoom GW] ";
 
 class BrowserLogger {
   contexts: [string, string][];
+  logger: LoggerType<ILogObj>;
 
   constructor(source: ExtensionFileSource) {
     this.contexts = [["initiator", source]];
+    this.logger = new Logger({
+      prefix: [PREFIX],
+      hideLogPositionForProduction: true
+    });
+
+    this.info("test");
   }
 
   public addContext(key: string, value: string) {
@@ -23,16 +34,18 @@ class BrowserLogger {
       return acc;
     }, {});
 
-    console.log(
-      `%c${PREFIX}%c${message} - %c${JSON.stringify(context, null, 2)}`,
-      "color: blue;font-weight: bold;",
-      "color: green;",
-      "color: red;"
-    );
+    this.logger.info(message, context);
+
+    // console.log(
+    //   `%c${PREFIX}%c${message} - %c${JSON.stringify(context, null, 2)}`,
+    //   "color: blue;font-weight: bold;",
+    //   "color: green;",
+    //   "color: red;"
+    // );
   }
 
   public error(error: Error) {
-    console.error(error);
+    this.logger.error(error);
   }
 }
 
