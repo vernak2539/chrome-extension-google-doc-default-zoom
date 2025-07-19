755a30f7ede98d7940bbc713521021399221a7e6
Alex Vernacchia <vernak2539@users.noreply.github.com>
2025-07-19 12:50:54 +0100

    chore: prettier storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,18 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 
-  addons: ["@storybook/addon-links", "@storybook/addon-a11y", "@storybook/addon-docs"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs"
+  ],
 
   framework: {
     name: "@storybook/react-vite",
     options: {}
   }
-};
-export default config;
+}
+
+export default config
