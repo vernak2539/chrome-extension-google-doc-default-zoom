c81fab96399870e8f314b4f3a026a774c1d233e6
Alex Vernacchia <vernak2539@users.noreply.github.com>
2025-07-22 20:48:56 +0100

    mock chrome localisation in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,11 @@
 import type { Preview } from "@storybook/react-vite";
+import englishMessages from "../locales/en/messages.json";
+
+window.chrome = window.chrome || {};
+window.chrome.i18n = window.chrome.i18n || {};
+window.chrome.i18n.getMessage = (key: string) => {
+  return englishMessages[key]?.message || key;
+};
 
 const preview: Preview = {
   parameters: {
